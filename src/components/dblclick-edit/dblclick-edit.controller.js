6183bc84d559fb567ba1335bfbfb9757e3b472a4
Mathias Karstädt <mathias.karstaedt@gmail.com>
2016-03-02 00:55:55 +0100

    simplyfied dblclick-edit component

--- src/components/dblclick-edit/dblclick-edit.controller.js
+++ src/components/dblclick-edit/dblclick-edit.controller.js
@@ -1,13 +1,11 @@
 export default function DblClickEditController () {
   this.editMode = false
-  this.tempText = null
   this.toggleEdit = function () {
     this.editMode = !this.editMode
-    this.tempText = this.text
   }
   this.toggleEditOnEnter = function ($event) {
     if ($event.keyCode === 13) {
-      this.onChange({text: this.tempText})
+      this.onChange({text: this.text})
       this.toggleEdit()
     }
   }
