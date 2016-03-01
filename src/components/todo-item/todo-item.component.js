9e12a954cfc76490dfba5f7af36fd1bd764967bc
Mathias Karstädt <mathias.karstaedt@gmail.com>
2016-03-02 00:15:45 +0100

    changed item class handling

--- src/components/todo-item/todo-item.component.js
+++ src/components/todo-item/todo-item.component.js
@@ -3,6 +3,9 @@ import angular from 'angular'
 // Template
 import todoItemTemplate from './todo-item.template.html!text'
 
+// Styles
+import './todo-item.styles.css!'
+
 // own stuff
 import TodoItemController from './todo-item.controller.js'
 
--- src/components/todo-item/todo-item.controller.js
+++ src/components/todo-item/todo-item.controller.js
@@ -1,2 +1,7 @@
+import {extendObservable} from 'mobx'
+
 export default function TodoItemController () {
+  extendObservable(this.item, {itemClass: function () {
+    return this.finished ? 'done' : ''
+  }})
 }
