4fad84cc5fa9bd92b8f2fe4cb358df0181e2eb68
Mathias Karstädt <mathias.karstaedt@gmail.com>
2016-03-01 17:40:55 +0100

    added material layout

--- src/components/todo-list/todo-list.component.js
+++ src/components/todo-list/todo-list.component.js
@@ -3,6 +3,9 @@ import angular from 'angular'
 // Template
 import todoListTemplate from './todo-list.template.html!text'
 
+// Styles
+import './todo-list.styles.css!'
+
 // Controller
 import TodoListController from './todo-list.controller.js'
 
