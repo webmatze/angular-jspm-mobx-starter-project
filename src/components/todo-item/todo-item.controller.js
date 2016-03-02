3cd37dcee94970ef8a8f6768bf4307c58911b522
Mathias Karstädt <mathias.karstaedt@gmail.com>
2016-03-02 17:14:26 +0100

    using $onInit

--- src/components/todo-item/todo-item.controller.js
+++ src/components/todo-item/todo-item.controller.js
@@ -1,9 +1,11 @@
 import {extendObservable} from 'mobx'
 
 export default function TodoItemController () {
-  extendObservable(this.item, {itemClass: function () {
-    return this.finished ? 'done' : ''
-  }})
+  this.$onInit = function () {
+    extendObservable(this.item, {itemClass: function () {
+      return this.finished ? 'done' : ''
+    }})
+  }
   this.setName = function (text) {
     this.item.name = text
   }
