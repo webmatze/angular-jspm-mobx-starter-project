5013a2743304687eaf8c06ff2e5de2482e893f7c
Mathias Karstädt <mathias.karstaedt@gmail.com>
2016-03-19 17:19:21 +0100

    using npm:angular and npm:angular-material instead of bower versions
    This closes #3

--- src/config.js
+++ src/config.js
@@ -14,37 +14,30 @@ System.config({
   },
 
   map: {
-    "angular": "github:angular/bower-angular@1.5.0",
-    "angular-material": "github:angular/bower-material@1.0.6",
-    "babel": "npm:babel-core@5.8.34",
-    "babel-runtime": "npm:babel-runtime@5.8.34",
-    "clean-css": "npm:clean-css@3.4.8",
+    "angular": "npm:angular@1.5.2",
+    "angular-material": "npm:angular-material@1.0.6",
+    "babel": "npm:babel-core@5.8.35",
+    "babel-runtime": "npm:babel-runtime@5.8.35",
+    "clean-css": "npm:clean-css@3.4.10",
     "core-js": "npm:core-js@1.2.6",
     "css": "github:systemjs/plugin-css@0.1.20",
     "json": "github:systemjs/plugin-json@0.1.0",
-    "mobx": "npm:mobx@2.0.0",
+    "mobx": "npm:mobx@2.0.2",
     "text": "github:systemjs/plugin-text@0.0.4",
-    "github:angular/bower-angular-animate@1.5.0": {
-      "angular": "github:angular/bower-angular@1.5.0"
+    "github:angular/bower-angular-animate@1.5.2": {
+      "angular": "github:angular/bower-angular@1.5.2"
     },
-    "github:angular/bower-angular-aria@1.5.0": {
-      "angular": "github:angular/bower-angular@1.5.0"
+    "github:angular/bower-angular-aria@1.5.2": {
+      "angular": "github:angular/bower-angular@1.5.2"
     },
-    "github:angular/bower-angular-messages@1.5.0": {
-      "angular": "github:angular/bower-angular@1.5.0"
-    },
-    "github:angular/bower-material@1.0.6": {
-      "angular": "github:angular/bower-angular@1.5.0",
-      "angular-animate": "github:angular/bower-angular-animate@1.5.0",
-      "angular-aria": "github:angular/bower-angular-aria@1.5.0",
-      "angular-messages": "github:angular/bower-angular-messages@1.5.0",
-      "css": "github:systemjs/plugin-css@0.1.20"
+    "github:angular/bower-angular-messages@1.5.2": {
+      "angular": "github:angular/bower-angular@1.5.2"
     },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
     "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.5.5"
+      "buffer": "npm:buffer@3.6.0"
     },
     "github:jspm/nodelibs-events@0.1.1": {
       "events": "npm:events@1.0.2"
@@ -84,13 +77,20 @@ System.config({
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
+    "npm:angular-material@1.0.6": {
+      "angular": "github:angular/bower-angular@1.5.2",
+      "angular-animate": "github:angular/bower-angular-animate@1.5.2",
+      "angular-aria": "github:angular/bower-angular-aria@1.5.2",
+      "angular-messages": "github:angular/bower-angular-messages@1.5.2",
+      "css": "github:systemjs/plugin-css@0.1.20"
+    },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:babel-runtime@5.8.34": {
+    "npm:babel-runtime@5.8.35": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:buffer@3.5.5": {
+    "npm:buffer@3.6.0": {
       "base64-js": "npm:base64-js@0.0.8",
       "child_process": "github:jspm/nodelibs-child_process@0.1.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
@@ -98,7 +98,7 @@ System.config({
       "isarray": "npm:isarray@1.0.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:clean-css@3.4.8": {
+    "npm:clean-css@3.4.10": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
       "commander": "npm:commander@2.8.1",
       "fs": "github:jspm/nodelibs-fs@0.1.2",
@@ -136,6 +136,9 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:isarray@1.0.0": {
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:os-browserify@0.1.2": {
       "os": "github:jspm/nodelibs-os@0.1.0"
